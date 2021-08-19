aac5161e266fd7d3322453ce038ffddc0cd2d077
Yusuke Kaneko <y.kaneko@4nonome.com>
2021-08-19 15:59:04 +0900

    fix apps/registration/tests.py, views.py, and urls.py

--- apps/registration/views.py
+++ apps/registration/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 
 from .forms import SignUpForm
@@ -8,3 +10,7 @@ class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('apps.registration:login')
     template_name = 'registration/signup.html'
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'registration/index.html'
+
