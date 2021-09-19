5a336f0fefa002b414fb4392d69e948ba8cfa633
Yusuke Kaneko <y.kaneko@4nonome.com>
2021-09-19 17:08:31 +0900

    twitter_clone, add like app

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,12 +7,12 @@ from . import views
 app_name = 'apps.users'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('', include('apps.tweet.urls')),
-    path('follow/<int:pk>', views.FollowInUserProfile.as_view(), name='follow_in_profile'),
+    path('follow/<int:pk>/', views.FollowInUserProfile.as_view(), name='follow_in_profile'),
     path('<str:username>/', views.UserProfileView.as_view(), name='profile'),
-    path('<str:username>/following', views.FollowingListView.as_view(), name='following_list'),
-    path('<str:username>/followers', views.FollowersListView.as_view(), name='followers_list'),
+    path('<str:username>/following/', views.FollowingListView.as_view(), name='following_list'),
+    path('<str:username>/followers/', views.FollowersListView.as_view(), name='followers_list'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -14,29 +14,41 @@ from .models import Connection, User
 class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('apps.users:login')
-    template_name = 'registration/signup.html'
+    template_name = 'users/registration/signup.html'
 
 
 class UserProfileView(LoginRequiredMixin, View):
   
     def get(self, request, *args, **kwargs):
-      user_data = get_object_or_404(User, username=self.kwargs["username"])
-      post_data = Post.objects.filter(user__username=self.kwargs["username"])
+      user_data = get_object_or_404(User, username=self.kwargs['username'])
+      post_data = Post.objects.filter(user__username=self.kwargs['username'])
       following_data =  Connection.objects.filter(user=user_data).values_list('followee')
       following_count = User.objects.filter(id__in=following_data).count()
-      followers_data = User.objects.get(username=self.kwargs["username"]).follower.all()
+      followers_data = User.objects.get(username=self.kwargs['username']).follower.all()
       followers_count = followers_data.count()
       request_user_following_data = Connection.objects.filter(user=self.request.user).values_list('followee')
-      request_user_following_list = User.objects.filter(id__in=request_user_following_data)
-      context = {'user_data':user_data, 'post_data':post_data, 'following_count':following_count, 'followers_count':followers_count ,'request_user_following_list':request_user_following_list}
-      return render(request, 'users/profile.html', context)
+      request_user_following_list = User.objects.filter(id__in=request_user_following_data) 
+      liked_list = []
+      for post in post_data:
+         liked = post.like_set.filter(user=self.request.user)
+         if liked.exists():
+               liked_list.append(post.pk)
+      context = {
+        'user_data':user_data, 
+        'post_data':post_data, 
+        'following_count':following_count, 
+        'followers_count':followers_count, 
+        'request_user_following_list':request_user_following_list,
+        'liked_list': liked_list,
+      }
+      return render(request, 'users/profile/profile.html', context)
 
 
 class FollowBase(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         follower = Connection.objects.get_or_create(user=self.request.user)
-        followee = get_object_or_404(User, pk=self.kwargs["pk"])
+        followee = get_object_or_404(User, pk=self.kwargs['pk'])
     
         if follower[0].user.username == followee.username:
             messages.error(request, '自分をフォローすることはできません') 
@@ -50,17 +62,17 @@ class FollowInUserProfile(FollowBase):
 
     def get(self, request, *args, **kwargs):
         super().get(request, *args, **kwargs)
-        user = User.objects.get(pk=self.kwargs["pk"])
+        user = User.objects.get(pk=self.kwargs['pk'])
         return redirect(reverse('apps.users:profile', kwargs={'username':user.username}))
 
 
 class FollowingListView(LoginRequiredMixin, ListView):
     model = Connection
-    template_name = 'users/following_list.html'
+    template_name = 'users/profile/following_list.html'
 
     def get_context_data(self, *args, **kwargs):
       context = super().get_context_data(*args, **kwargs)
-      user =  get_object_or_404(User, username=self.kwargs["username"])
+      user =  get_object_or_404(User, username=self.kwargs['username'])
       following = Connection.objects.filter(user=user).values_list('followee')
       context['following_list'] = User.objects.filter(id__in=following)
       return context
@@ -68,9 +80,9 @@ class FollowingListView(LoginRequiredMixin, ListView):
 
 class FollowersListView(LoginRequiredMixin, ListView):
     model = Connection
-    template_name = 'users/followers_list.html'
+    template_name = 'users/profile/followers_list.html'
 
     def get_context_data(self, *args, **kwargs):
       context = super().get_context_data(*args, **kwargs)
-      context['followers_list'] = get_object_or_404(User, username=self.kwargs["username"]).follower.all()
+      context['followers_list'] = get_object_or_404(User, username=self.kwargs['username']).follower.all()
       return context
